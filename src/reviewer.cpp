ba7793d6a52c44563d764af998a336c30d5746ae
Jett Thompson <jett.thompson.59@gmail.com>
2021-11-12 16:29:39 +1100

    made the program print out some random names

--- src/reviewer.cpp
+++ src/reviewer.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
+#include <fstream>
 
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::ifstream;
+using std::ios;
 using std::string;
+using std::unique_ptr;
 using std::vector;
 
 vector<string> grabWords(string input) {
@@ -29,5 +35,17 @@ vector<string> grabWords(string input) {
 
 int main() { 
   cout << "Book reviewer" << endl;
+  ifstream namesStream("names.txt");
+  if(!namesStream) { 
+    cerr << "Error while opening file handle" << endl;
+    return -1;
+  }
+  namesStream.seekg(0, ios::end);
+  int fileSize = namesStream.tellg();
+  namesStream.seekg(0, ios::beg);
+  unique_ptr<char> namesData(new char[fileSize]);
+  namesStream.read(namesData.get(), fileSize);
+  cout.write(namesData.get(), fileSize);
+  namesStream.close();
   return 0;
 }
