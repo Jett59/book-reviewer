7156b09c69ec7adcfb9ba31b0f4d5b95d340919d
Jett Thompson <jett.thompson.59@gmail.com>
2021-11-15 17:19:37 +1100

    mad e the program generate a random title

--- src/reviewer.js
+++ src/reviewer.js
@@ -3,6 +3,20 @@ async function readFile(path) {
     return response.text();
 }
 
+async function readWords(path) {
+    return (await readFile(path)).split('\n');
+}
+
+function randomInt(bound) {
+    return Math.round(Math.random() * bound);
+}
+
 async function generateReview(output) {
-    output.innerHTML = await readFile('5stars.txt');
+    const names = await readWords('names.txt');
+    let name1 = names[randomInt(names.length - 1)];
+    let name2 = names[randomInt(names.length - 1)];
+    const nouns = await readWords('nouns.txt');
+    let noun1 = nouns[randomInt((await nouns).length - 1)];
+    let noun2 = nouns[randomInt((await nouns).length - 1)];
+    output.innerHTML = name1 + ' ' + name2 + ' and the ' + noun1 + ' of ' + noun2;
 }
